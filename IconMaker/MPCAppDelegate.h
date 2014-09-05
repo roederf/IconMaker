1817920dd270328cd2347dc2b267068c14aac83e
Florian Röder <florian@amplifycamp.com>
2014-09-05 15:49:19 +0200

    Initial Commit

---
+++ IconMaker/MPCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MPCAppDelegate.h
+//  IconMaker
+//
+//  Created by Florian Roeder on 9/5/14.
+//  Copyright (c) 2014 amplify. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MPCAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ IconMaker/MPCAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  MPCAppDelegate.m
+//  IconMaker
+//
+//  Created by Florian Roeder on 9/5/14.
+//  Copyright (c) 2014 amplify. All rights reserved.
+//
+
+#import "MPCAppDelegate.h"
+
+@implementation MPCAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
